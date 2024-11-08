ef6ecb7ca4c333cea0491d88d4a1132ffd93f9e8
ROKLJH <eltmzm8358@gmail.com>
2024-11-08 19:13:22 +0900

    WhileEx

---
+++ 01_java/global/MyUtil.java
@@ -0,0 +1,12 @@
+package util;
+
+public class MyUtil {
+	public static String RED   = "\033[91m";
+	public static String GREEN = "\033[92m";
+	public static String END   = "\033[0m";
+	
+	public static void p(String str) {
+		System.out.println(str);
+	}
+	
+}
