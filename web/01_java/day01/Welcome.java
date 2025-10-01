f83cf28a7e345c9423e2f1de62ab144c99dc1375
ROKLJH <eltmzm8358@gmail.com>
2025-10-01 19:41:41 +0900

    welcome add

---
+++ web/01_java/day01/Welcome.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my house");
+        System.out.println("\033[91m" + "Welcome to my house" + "\033[0m");
+        System.out.print("Welcome to your house");
+        System.out.print("Welcome to his house");
+    }
+}
